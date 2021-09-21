42e79b5ebda9fc7ff25a6dd3271c37aaf01ceb02
happiestlife <chickenman10@naver.com>
2021-09-21 12:48:51 +0900

    Strategy pattern

---
+++ src/Sorter.java
@@ -0,0 +1,20 @@
+public class Sorter {
+    private Comparable compare;
+    public Sorter(Comparable comparable){
+        this.compare = comparable;
+    }
+    public void setComparable(Comparable comparable){
+        this.compare = comparable;
+    }
+    public void bubbleSort(Object[] data){
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = 0; j < data.length - i - 1; j++) {
+                if(compare.compareTo(data[j], data[j+1]) > 0){
+                    Object tmp = data[j];
+                    data[j] = data[j+1];
+                    data[j+1] = tmp;
+                }
+            }
+        }
+    }
+}
